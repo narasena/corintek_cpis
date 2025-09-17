1e9d4433e816234c656a4984fe3befc3bb9d8858
narasena <cursedsena@gmail.com>
2025-09-17 15:52:19 +0700

    feat: set custom prisma path in config

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import path from 'node:path'
+import type { PrismaConfig } from 'prisma'
+
+export default {
+  schema: path.join(__dirname, 'src', 'app', 'api', 'prisma', 'schema.prisma'),
+  migrations: {
+    path: path.join(__dirname, 'src', 'app', 'api', 'prisma', "migrations"),
+  },
+} satisfies PrismaConfig
