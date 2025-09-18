c265dadb91e11eda646cc6f8bc7db04152c93dff
narasena <cursedsena@gmail.com>
2025-09-18 19:49:25 +0700

    feat: login page component by shadcn

---
+++ src/app/login/page.tsx
@@ -0,0 +1,19 @@
+import { LoginForm } from '@/components/login-form';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <div className="flex flex-col min-h-svh w-full bg-blue-100 items-center justify-center sm:p-6 md:p-10 gap-y-6">
+      <Image
+        src={'/logo.png'}
+        alt="Logo"
+        width={300}
+        height={150}
+        className="object-contain"
+      />
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
