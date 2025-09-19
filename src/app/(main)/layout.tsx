92cf2cea9c570febbfe0b147145979353dcb56f2
narasena <cursedsena@gmail.com>
2025-09-19 20:57:02 +0700

    refactor: basic main layout and remove next home page

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,27 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function MainLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+    );
+}
