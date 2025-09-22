683649380676d402df806d871d560f13c9f3daa2
narasena <cursedsena@gmail.com>
2025-09-22 16:32:41 +0700

    feat: create user types

---
+++ src/app/types/user.type.ts
@@ -0,0 +1,7 @@
+import z from "zod"
+import { ITableHelper } from "./table.helper.type"
+import userSchema from "../(main)/users/schemas/userSchema"
+
+type TUserAttributes = z.infer<typeof userSchema>
+
+export interface IUser extends TUserAttributes, ITableHelper {}
