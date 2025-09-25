4f7b676751ef5908cea5e9886a440357534f9618
narasena <cursedsena@gmail.com>
2025-09-25 21:43:19 +0700

    fix: user zod schema validation

--- src/app/(main)/users/schemas/userSchema.ts
+++ src/app/(main)/users/schemas/userSchema.ts
@@ -2,17 +2,18 @@ import { EmploymentStatus, UserRole } from "@/app/api/generated/prisma";
 import z from "zod";
 
 const userSchema = z.object({
-  firstName: z.string(),
+  firstName: z.string("Hanya huruf").nonempty("Wajib diisi").min(3, "Minimal 3 huruf"),
   lastName: z.string().optional(),
   IDNumber: z.string(),
-  email: z.email(),
-  phoneNumber: z.string(),
-  role: z.enum(UserRole),
-  employmentStatus: z.enum(EmploymentStatus),
+  email: z.email("Format email salah").nonempty("Wajib diisi"),
+  password: z.string().nonempty("Wajib diisi"),
+  phoneNumber: z.string("Hanya angka").regex(/^[0-9]+$/, "Hanya angka").min(10, "Minimal 10 angka").max(17, "Maksimal 17 angka"),
+  role: z.enum(UserRole,"Pilih salah satu"),
+  employmentStatus: z.enum(EmploymentStatus,"Pilih salah satu"),
   avatarUrl: z.string().optional(),
   avatarPublicId: z.string().optional(),
-  isActive: z.boolean(),
-  isBlocked: z.boolean(),
+  isActive: z.boolean().optional(),
+  isBlocked: z.boolean().optional(),
 })
 
 export default userSchema
