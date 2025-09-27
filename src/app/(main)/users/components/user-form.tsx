c5b9b8b766bce377785c055bf24c2df6f7278d46
narasena <cursedsena@gmail.com>
2025-09-27 08:22:23 +0700

    refactor: form fields

--- src/app/(main)/users/components/user-form.tsx
+++ src/app/(main)/users/components/user-form.tsx
@@ -37,6 +37,8 @@ import z from 'zod';
 
 import { Checkbox } from '@/components/ui/checkbox';
 import { Label } from '@/components/ui/label';
+import useImageUpload from '@/hooks/useImageUpload';
+import { formFields } from '../data/formFiels';
 
 export default function UserForm() {
   const form = useForm<TUserAttributes>({
@@ -58,89 +60,7 @@ export default function UserForm() {
   });
 
   const { previewUrl, handleImagePreview } = useImagePreview<TUserAttributes, 'avatarUrl'>();
-
-  const formFields = [
-    {
-      name: 'firstName',
-      type: 'text',
-      label: 'Nama Depan',
-      placeHolder: 'John',
-      description:
-        'Masukkan nama depan sesuai dengan yang terdaftar di perusahaan Anda bekerja',
-    },
-    {
-      name: 'lastName',
-      type: 'text',
-      label: 'Nama Belakang',
-      placeHolder: 'Doe',
-      description:
-        'Masukkan nama belakang sesuai dengan yang terdaftar di perusahaan Anda bekerja',
-    },
-    {
-      name: 'idNumber',
-      type: 'text',
-      label: 'ID Number',
-      placeHolder: 'ID-12345',
-      description:
-        'Masukkan nomor identitas sesuai dengan yang terdaftar di perusahaan Anda bekerja',
-    },
-    {
-      name: 'phoneNumber',
-      type: 'text',
-      label: 'Nomor Telepon',
-      placeHolder: '088812345678',
-      description:
-        'Masukkan nomor telepon sesuai dengan yang terdaftar di perusahaan Anda bekerja',
-    },
-    {
-      name: 'email',
-      type: 'email',
-      label: 'Email',
-      placeHolder: 'Y2mE2@example.com',
-      description:
-        'Masukkan email sesuai dengan yang terdaftar di perusahaan Anda bekerja',
-    },
-    {
-      name: 'password',
-      type: 'password',
-      label: 'Password',
-      placeHolder: 'Password',
-      description: "Buat password untuk user"
-    },
-    {
-      name: 'role',
-      type: 'selectEnum',
-      label: 'Role',
-      placeHolder: 'TECHNICIAN',
-      description:
-        'Masukkan role sesuai dengan yang terdaftar di perusahaan Anda bekerja',
-    },
-    {
-      name: 'employmentStatus',
-      type: 'selectEnum',
-      label: 'Employment Status',
-      placeHolder: 'PERMANENT',
-      description:
-        'Masukkan status kerja sesuai dengan yang terdaftar di perusahaan Anda bekerja',
-    },
-    {
-      name: 'isActive',
-      type: 'boolean',
-      label: 'Aktif',
-      placeHolder: 'true',
-      description:
-        'Masih aktif bekerja',
-    },
-    {
-      name: 'isBlocked',
-      icon: IconLockFilled,
-      type: 'boolean',
-      label: 'Blokir',
-      placeHolder: 'false',
-      description:
-        'Blokir / batasi akses user / pengguna',
-    },
-  ];
+  const {file,handleUpload,result,setFile,uploading} = useImageUpload()  
 
   const onSubmit = (data: TUserAttributes) => {
     console.log('onSubmit called');
