9749377cd9947601ff7ed0a48e0d4640cdfdaf5e
narasena <cursedsena@gmail.com>
2025-09-27 20:29:49 +0700

    refactor: separate user schema to creation and edit

--- src/app/(main)/users/schemas/userSchema.ts
+++ src/app/(main)/users/schemas/userSchema.ts
@@ -1,19 +1,20 @@
 import { EmploymentStatus, UserRole } from "@/features/api/generated/prisma";
 import z from "zod";
+import { is } from "zod/v4/locales";
 
-const userSchema = z.object({
+export const userCreationSchema = z.object({
   firstName: z.string("Hanya huruf").regex(/^[a-zA-Z]+$/, "Hanya huruf").nonempty("Wajib diisi").min(3, "Minimal 3 karakter"),
-  lastName: z.string().regex(/^[a-zA-Z]+$/, "Hanya huruf").nullable().optional(),
+  lastName: z.string().regex(/^[a-zA-Z]+$/, "Hanya huruf").nonempty("Wajib diisi"),
   idNumber: z.string().nullable().optional(),
   email: z.email("Format email salah").nonempty("Wajib diisi"),
   password: z.string().nonempty("Wajib diisi").min(6, "Minimal 6 karakter"),
   phoneNumber: z.string("Hanya angka").regex(/^[0-9]+$/, "Hanya angka").min(10, "Minimal 10 angka").max(17, "Maksimal 17 angka"),
   role: z.enum(UserRole,"Pilih salah satu"),
   employmentStatus: z.enum(EmploymentStatus,"Pilih salah satu"),
-  avatarUrl: z.url().nullable().optional(),
-  avatarPublicId: z.string().nullable().optional(),
-  isActive: z.boolean().optional(),
-  isBlocked: z.boolean().optional()
+  avatarImg: z.file().nullable().optional(),
 })
 
-export default userSchema
+export const userEditSchema = userCreationSchema.extend({
+  isActive: z.boolean().nullable().optional(),
+  isBlocked: z.boolean().nullable().optional(),
+})
