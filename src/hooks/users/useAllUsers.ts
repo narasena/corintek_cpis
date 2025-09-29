34ff2ba072f468d72c643f4a8dda6806819c0183
narasena <cursedsena@gmail.com>
2025-09-29 13:17:15 +0700

    feat: add get all users hook

---
+++ src/hooks/users/useAllUsers.ts
@@ -0,0 +1,24 @@
+import { IUser } from "@/types/user.type";
+import apiInstance from "@/utils/apiInstance";
+import errorMessageResponse from "@/utils/errorMessageResponse";
+import React from "react";
+
+export default function useAllUsers() {
+  const [allUsers, setAllUsers] = React.useState<IUser[]>([]);
+
+  const fetchAllUsers = async () => {
+    try {
+      const response = await apiInstance.get("/users");
+      setAllUsers(response.data.users);
+    } catch (error) {
+      errorMessageResponse(error);
+      console.error(error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
+  return { allUsers };
+}
