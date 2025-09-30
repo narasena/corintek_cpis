22c5a27b443657f2cc4eab4cfbbeaf57cf3d76d7
narasena <cursedsena@gmail.com>
2025-09-30 13:05:42 +0700

    feat: add project validation schema

---
+++ src/app/(main)/projects/schemas/projectSchema.ts
@@ -0,0 +1,19 @@
+import { ContractType, ProjectType, WorkCategory } from "@/features/api/generated/prisma";
+import z from "zod";
+
+export const projectCreationSchema = z.object({
+  parentId: z.string().nonempty(),
+  clientId: z.string().nonempty(),
+  name: z.string().nonempty(),
+  description: z.string().nullable().optional(),
+  quoteNumber: z.string().nonempty(),
+  PONumber: z.string().nonempty(),
+  startDate: z.date(),
+  endDate: z.date(),
+  type: z.enum(ProjectType),
+  contractType: z.enum(ContractType),
+  workCategory: z.enum(WorkCategory),
+  warranty: z.number().nullable().optional(),
+  clientPICId: z.string().nonempty(),
+  technicianId: z.string().nonempty(),
+})
