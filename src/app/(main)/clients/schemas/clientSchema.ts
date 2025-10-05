f05548eb543c09b9411acc473db94adccc87cf1c
narasena <cursedsena@gmail.com>
2025-10-05 09:10:22 +0700

    fix: preprocess blank field schema

--- src/app/(main)/clients/schemas/clientSchema.ts
+++ src/app/(main)/clients/schemas/clientSchema.ts
@@ -1,21 +1,35 @@
-import { description } from "@/components/chart-area-interactive";
-import { defaultSchemaMessage } from "@/features/schemas/defaultSchema";
-import z from "zod";
+import {
+  defaultSchemaMessage,
+  preprocessBlank,
+} from '@/features/schemas/defaultSchema';
+import z from 'zod';
 
 export const clientCreationSchema = z.object({
-  name: z.string(defaultSchemaMessage.only.alphabet).nonempty(
-    defaultSchemaMessage.nonempty,
+  name: z
+    .string(defaultSchemaMessage.only.alphabet)
+    .nonempty(defaultSchemaMessage.nonempty),
+  email: preprocessBlank(
+    z.email(defaultSchemaMessage.email).nullable().optional()
   ),
-  email: z.email(defaultSchemaMessage.email).nullable().optional(),
   description: z.string().nullable().optional(),
-  phoneNumber: z
-    .string(defaultSchemaMessage.only.number)
-    .regex(/^[0-9]+$/, { message: defaultSchemaMessage.only.number })
-    .min(10, defaultSchemaMessage.min(10))
-    .max(17, defaultSchemaMessage.max(17))
+  phoneNumber: preprocessBlank(
+    z
+      .string(defaultSchemaMessage.only.number)
+      .regex(/^[0-9]+$/, { message: defaultSchemaMessage.only.number })
+      .min(10, defaultSchemaMessage.min(10))
+      .max(17, defaultSchemaMessage.max(17))
+      .nullable()
+      .optional()
+  ),
+  address: z.string().nullable().optional(),
+  avatarImg: z
+    .file()
+    .max(1, defaultSchemaMessage.image_max_file(1))
+    .mime(
+      ['image/png', 'image/jpeg', 'image/jpg', 'image/webp'],
+      defaultSchemaMessage.supported_image_format
+    )
     .nullable()
     .optional(),
-  address: z.string().nullable().optional(),
-  avatarImg: z.file().max(1,defaultSchemaMessage.image_max_file(1)).mime(['image/png', 'image/jpeg', 'image/jpg', 'image/webp'], defaultSchemaMessage.supported_image_format).nullable().optional(),
-  websiteUrl: z.url().nullable().optional(),
+  websiteUrl: preprocessBlank(z.url().nullable().optional()),
 });
--- src/features/schemas/defaultSchema.ts
+++ src/features/schemas/defaultSchema.ts
@@ -1,21 +1,21 @@
-import { number } from "zod";
+import z from 'zod';
 
 export const defaultSchemaMessage = {
-  nonempty: "Wajib diisi",
-  email: "Format email salah",
-  regex: "Format salah",
-  choose_one: "Pilih salah satu",
+  nonempty: 'Wajib diisi',
+  email: 'Format email salah',
+  regex: 'Format salah',
+  choose_one: 'Pilih salah satu',
   only: {
-    alphabet: "Hanya huruf",
-    number: "Hanya angka",
+    alphabet: 'Hanya huruf',
+    number: 'Hanya angka',
   },
   at_least_one: {
-    character: "Minimal 1 karakter",
-    alphabet: "Minimal 1 huruf",
-    uppercase: "Minimal 1 huruf besar",
-    lowercase: "Minimal 1 huruf kecil",
-    number: "Minimal 1 angka",
-    special_character: "Minimal 1 karakter khusus",
+    character: 'Minimal 1 karakter',
+    alphabet: 'Minimal 1 huruf',
+    uppercase: 'Minimal 1 huruf besar',
+    lowercase: 'Minimal 1 huruf kecil',
+    number: 'Minimal 1 angka',
+    special_character: 'Minimal 1 karakter khusus',
   },
   min(num: number) {
     return { message: `Minimal ${num} karakter` };
@@ -23,9 +23,16 @@ export const defaultSchemaMessage = {
   max(num: number) {
     return { message: `Maksimal ${num} karakter` };
   },
-  passwordMismatch: "Password tidak sesuai",
-  supported_image_format: "Format gambar tidak didukung (jpg, jpeg, png, webp)",
-  image_max_file(num:number) {
-    return {message: `Maksimal hanya ${num} file` }
-  }
+  passwordMismatch: 'Password tidak sesuai',
+  supported_image_format: 'Format gambar tidak didukung (jpg, jpeg, png, webp)',
+  image_max_file(num: number) {
+    return { message: `Maksimal hanya ${num} file` };
+  },
+};
+
+export const preprocessBlank = (schema: z.ZodTypeAny) => {
+  return z.preprocess(val => {
+    if (val === undefined || val === null || val === '') return null;
+    return val;
+  }, schema);
 };
