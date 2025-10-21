b6b1d0ad6464ba05789d5083ce8019dfe89b1972
Narasena <cursedsena@gmail.com>
2025-10-21 12:48:54 +0700

    fix: npm run build

--- src/app/api/v1/projects/[id]/log-sheets/route.ts
+++ src/app/api/v1/projects/[id]/log-sheets/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest } from 'next/server';
 
 export async function POST(
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  req: NextRequest
+  // { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = await params;
+  // const { id } = await params;
   const body = await req.json();
-  console.log(body)
+  console.log(body);
 }
